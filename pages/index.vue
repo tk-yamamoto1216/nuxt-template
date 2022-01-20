2f510c82c2d2e1c33d7e5a7fafd2540f7cb58fde
Takenari Yamamoto <takenari_yamamoto1216@arsaga.jp>
2022-01-21 08:19:58 +0900

    css設定

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@ import { defineComponent, ref } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   setup() {
-    const text = ref('This is nuxt project')
+    const text = ref('This is Nuxt project')
 
     return { text }
   },
