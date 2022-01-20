33e9f25cd198eced9a08fe987261d355c7e52956
Takenari Yamamoto <takenari_yamamoto1216@arsaga.jp>
2022-01-21 07:30:12 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ text }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    const text = ref('This is nuxt project')
+
+    return { text }
+  },
+})
+</script>
