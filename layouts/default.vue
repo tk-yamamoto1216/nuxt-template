57d61c59c446336ed29b3c37c205feaeff30503f
Takenari Yamamoto <takenari_yamamoto1216@arsaga.jp>
2022-02-12 22:51:06 +0900

    sfc設定

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <TheAppHeader />
+    <Nuxt class="layout-page" />
+  </div>
+</template>
+
+<script>
+import TheAppHeader from '@/components/atoms/AppHeader.vue'
+
+export default {
+  components: {
+    TheAppHeader,
+  },
+}
+</script>
--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="index">
-    {{ text }}
-  </div>
+  <div class="index">Datepicker Vee-validate</div>
 </template>
 
 <script lang="ts">
